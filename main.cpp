63a2132842d992e8d362db3b418ffa9af32e74e7
ysc3839 <yurichard3839@gmail.com>
2017-08-26 01:05:40 +0800

    Add main.cpp and main.h.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "EventHandler.h"
+#include "PythonModule.h"
+
+extern "C" EXPORT unsigned int VcmpPluginInit(PluginFuncs* pluginFunctions, PluginCallbacks* pluginCallbacks, PluginInfo* pluginInfo)
+{
+    pluginInfo->pluginVersion = 0x100;
+	pluginInfo->apiMajorVersion = PLUGIN_API_MAJOR;
+	pluginInfo->apiMinorVersion = PLUGIN_API_MINOR;
+    strcpy(pluginInfo->name, "vcmp-python-plugin");
+
+    vcmpFunctions = pluginFunctions;
+	RegisterCallbacks(pluginCallbacks);
+
+    py::initialize_interpreter();
+
+	return 1;
+}
---
+++ main.h
@@ -0,0 +1,32 @@
+#include <fstream>
+
+#include <pybind11/embed.h>
+namespace py = pybind11;
+
+#include "plugin.h"
+
+#ifdef _WIN32
+#define EXPORT __declspec(dllexport)
+
+#define strcpy strcpy_s
+#else
+#define EXPORT
+#endif
+
+PluginFuncs* vcmpFunctions;
+py::module* moduleCallbacks = nullptr;
+
+void PythonExceptionHandler()
+{
+	try
+	{
+		throw;
+	}
+	catch (py::error_already_set e)
+	{
+		e.restore();
+		PyErr_Print();
+		if (!e.matches(PyExc_KeyboardInterrupt))
+			vcmpFunctions->ShutdownServer();
+	}
+}
